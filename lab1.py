f1de12c054aa7c33effb6081afbaabdb5923b3fc
Hayden <zpenghern@gmil.com>
2026-09-04 11:25:11 +0800

    Interactive User Profile

--- lab1.py
+++ lab1.py
@@ -1,19 +1,10 @@
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print ("\nInstagram Profile")
 print ("===============================")
-print ("Welcome here!")
-print ("My First Post!")
+print ("Username:", username)
+print ("Age:", age)
+print ("Category:", category)
 print ("===============================")
-
-username = "aylmao"
-bio = "Fun Blogger"
-followers = 100
-
-print ("Username: ", username)
-print ("Bio: ", bio)
-print ("Followers:", followers)
-
-followers += 50
-print ("Day 1:", followers)
-followers += 20
-print ("Day 2:", followers)
-followers += 40
-print ("Day 3:", followers)
