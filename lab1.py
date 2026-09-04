df9fa0ec37a4f47ee129162d4be62fe50d261505
Hayden <zpenghern@gmil.com>
2026-09-04 11:14:24 +0800

    changes

--- lab1.py
+++ lab1.py
@@ -1,1 +1,12 @@
-print ("hi")
+print ("===============================")
+print ("Welcome here!")
+print ("My First Post!")
+print ("===============================")
+
+username = "aylmao"
+bio = "Fun Blogger"
+followers = 100
+
+print ("Username: ", username)
+print ("Bio: ", bio)
+print ("Followers:", followers)
