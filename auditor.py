2d70504f90ce2248bf2090ca475dc5f1e5bdc14b
Hayden <2602141@sit.singaporetech.edu.sg>
2026-09-11 13:43:22 +0800

    Final

--- auditor.py
+++ auditor.py
@@ -1,16 +1,18 @@
 quantity = 0
 valid_quan = 0
 total = 0
-stock = 0
+failed_ent = 0
 
 while True:
     quantity = input("Whats the quantity of the stock? (Enter 'quit' to exit): ")
 
     if quantity == "quit":
-        print ("Total stock is " +str(total))
+        print ("Total Units Processed is " +str(total))
+        print ("Number of Failed/Rejected Entries is " +str(failed_ent))
         break
 
     elif not quantity.isdigit() or quantity < "0":
+        failed_ent = failed_ent + 1
         print("Please enter valid number as an integer")
 
     else:
