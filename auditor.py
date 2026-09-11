4d871df3926b3c84495ff00367f6fd4fde7ac68f
Hayden <2602141@sit.singaporetech.edu.sg>
2026-09-11 13:26:17 +0800

    reject neg

--- auditor.py
+++ auditor.py
@@ -1,4 +1,6 @@
-inventory = 0
+quantity = 0
+real_quan = 0
+stock = 0
 
 while True:
     quantity = input("Whats the quantity of the stock? (Enter 'quit' to exit): ")
@@ -6,10 +8,12 @@ while True:
     if quantity == "quit":
         break
 
-    elif not quantity.isdigit():
+    elif not quantity.isdigit() or quantity < "0":
         print("Please enter valid number as an integer")
 
     else:
         real_quan = quantity
 
-print("Quantity is " +real_quan)
+stock = str(real_quan)
+
+print("Quantity is " +stock)
