8e98fddcd877bf099a3bde1c8bb44f731dce91a2
Hayden <2602141@sit.singaporetech.edu.sg>
2026-09-23 17:25:23 +0800

    process delivery

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,22 +1,37 @@
 def get_valid_input():
-    quantity = 0
-    total = 0
     failed_ent = 0
     while True:
         quantity = input("Whats the quantity of the stock? (Enter 'quit' to exit): ")
 
         if quantity == "quit":
-            print ("Total Units Processed is " +str(total))
-            print ("Number of Failed/Rejected Entries is " +str(failed_ent))
-            break
+            return quantity, failed_ent
 
         elif not quantity.isdigit():
-            failed_ent = failed_ent + 1
             print("Please enter valid number as an integer")
+            failed_ent = 1
+            quantity = 0
+            return int(quantity), failed_ent
 
         else:
-            total = total + int(quantity)
+            return int(quantity), failed_ent
 
-    return total 
+def process_delivery(current_total, new_value):
+    total = (current_total) + (new_value)
+    return total
 
-get_valid_input()
+total = 0
+total_fail = 0
+
+while True:
+    quantity, failed_ent = get_valid_input()
+    total_fail = total_fail + failed_ent
+    
+
+    if quantity == "quit":
+        print("Number of failed is ", total_fail)
+        break
+
+    total = process_delivery(total, quantity)
+
+
+print("Number of units processed is ", + total)
